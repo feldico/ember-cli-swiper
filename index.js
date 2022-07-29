e6261d999105ba8016b813abd0b3d899ec7c2444
Marcelo Mira <hello@marcemira.com>
2022-07-29 12:46:40 -0300

    Fix node_modules for monorepos

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const fs = require('fs')
+
+const nodeModules = require('find-node-modules')();
+const possibleLocations = nodeModules.map(path => `${path}/swiper`);
+const existentLocations = possibleLocations.filter(path =>
+  fs.existsSync(path)
+);
+
 module.exports = {
   name: 'ember-cli-swiper',
 
@@ -8,7 +16,7 @@ module.exports = {
       plugins: [require.resolve('ember-auto-import/babel-plugin')],
     },
     sassOptions: {
-      includePaths: ['node_modules/swiper'],
+      includePaths: existentLocations,
     },
   },
 };
